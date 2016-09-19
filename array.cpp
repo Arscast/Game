4b46583c435295152a4cc3fcc8c02956a8854980
Arseni Rakutska <r.ars1.ar@gmail.com>
2016-09-19 15:39:45 +0200

    Add array with countries

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    string countries[ 9 ] = { "russia","algeria","brazil","kanada","australia","pekin","istanbul","brasilia","mexico"};
+    
+    for (int i = 0; i < 9; i++)
+        
+    {cout << countries[i] << endl;}
+           
+    return 0;
+
+}
